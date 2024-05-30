2d5a9d0910c07ef2fb811864ee050de4ab65fde7
Ahrorbek05 <akramjonovahrorbek@gmail.com>
2024-05-30 08:58:18 +0500

    home work

---
+++ js/algorithm.js
@@ -0,0 +1,74 @@
+// 1-masala
+// let = +prompt(`1 dan 50 gacha bo'lgan ixtiyoriy son kiriting`);
+// for ( let i = 1; i <= 50; i++){
+//     if ( i % 2 !== 0){
+//         console.log(i**2);
+//     }
+// }
+
+// 2-masala
+// let a = +prompt(`1 dan 10 gacha ixtiyoriy son kiriting`);
+// for ( i = 1; i <= 10; i++){
+//     console.log(i**3);
+// }
+
+// 3-masala
+// let n = +prompt(`1 dan 20 gacha ixtiyoriy son kiriting`);
+// for (let i = 1; i <= 20; i++) {
+//     let factorial = 1;
+//     for (let n = 1; n <= i; n++) {
+//       factorial *= n;
+//     }
+//     console.log(`faktorial ${i} hisoblanadi ${factorial}`);
+//   }
+
+// 4-masala
+// let y = +prompt(`1 dan 30 gacha ixtiyoriy son kiriting`);
+// let sum = 0;
+// for ( i = 1; i <= 30; i+=1){
+//     sum = sum + i**2;
+//     console.log(sum);
+// }
+
+// 5-masala
+// for ( i = 1; i <= 20; i++){
+//     let count = 0;
+//     for ( let n = 1; n <= i; n++){
+//         if( i % n == 0){
+//             count = count + 1;
+//         }
+//     }
+//     if (count == 2){
+//         console.log(i);
+//     }
+// }
+
+// 6-masala
+// let son = [];
+// for (let i = 1; i <= 100; i++) {
+//     if (i % 7 === 0 && i % 5 !== 0) {
+//         son.push(i);
+//     }
+// }
+// console.log(son);
+
+// 7-masala
+// let sonlar = [];
+// for (let i = 1; i <= 100; i++) {
+//     if (i * 2 < 100) {
+//         sonlar.push(i);
+//     }
+// }
+// console.log(sonlar);
+
+
+// 8-masala
+// let n = +prompt("son kiriting");
+// let summ = 0;
+// for( let i = 1; i <= n; i++){
+//    if (n % i == 0 ){
+//     summ = summ + i
+//     console.log(i);
+// }
+// }
+// console.log(`yig'indi ${summ}`); 
